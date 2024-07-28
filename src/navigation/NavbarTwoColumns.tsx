948f0d9c871330f9477b213388c5f5ba9dec0767
Nikita Kolchin <kolchin.nv@gmail.com>
2024-07-28 16:54:57 +0300

    improved design +

--- src/navigation/NavbarTwoColumns.tsx
+++ src/navigation/NavbarTwoColumns.tsx
@@ -2,23 +2,22 @@ import Link from 'next/link';
 import type { ReactNode } from 'react';
 
 type INavbarProps = {
-  logo: ReactNode;
-  children: ReactNode;
+    logo: ReactNode;
+    children: ReactNode;
 };
 
 const NavbarTwoColumns = (props: INavbarProps) => (
-  <div className="flex flex-wrap items-center justify-between">
-    <div>
-      <Link href="/">{props.logo}</Link>
+    <div className="flex flex-wrap items-center justify-between">
+        <div>
+            <Link href="/">{props.logo}</Link>
+        </div>
+
+        <nav>
+            <ul className="navbar flex items-center text-gray-800">
+                {props.children}
+            </ul>
+        </nav>
     </div>
-
-    <nav>
-      <ul className="navbar flex items-center text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
-
-  </div>
 );
 
 export { NavbarTwoColumns };
