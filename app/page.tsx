b1fec046afe818f0e9aaffe275db5ff585e5b193
Nikita Kolchin <kolchin.nv@gmail.com>
2024-08-02 11:31:43 +0300

    comment tables

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Banner } from '@/components/templates/Banner';
 import { Hero } from '@/components/templates/Hero';
 import { Format } from '@/components/templates/Format';
 import { Help } from '@/components/templates/Help';
-import { Education } from '@/components/templates/Education';
-import { Experience } from '@/components/templates/Experience';
-import { Achievements } from '@/components/templates/Achievements';
-import { Numbers } from '@/components/templates/Numbers';
+// import { Education } from '@/components/templates/Education';
+// import { Experience } from '@/components/templates/Experience';
+// import { Achievements } from '@/components/templates/Achievements';
+// import { Numbers } from '@/components/templates/Numbers';
 
 export default function Home() {
     return (
@@ -14,10 +14,10 @@ export default function Home() {
             <Format />
             <Help />
             <Banner />
-            <Education />
+            {/* <Education />
             <Experience />
             <Numbers />
-            <Achievements />
+            <Achievements /> */}
         </main>
     );
 }
