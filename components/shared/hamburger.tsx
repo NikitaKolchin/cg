a5e298d4d48ced0a11a897c0b0a0b5b8beb14204
Nikita Kolchin <kolchin.nv@gmail.com>
2024-09-10 17:42:22 +0300

    add hamburger state

--- components/shared/hamburger.tsx
+++ components/shared/hamburger.tsx
@@ -1,17 +1,25 @@
 'use client';
 
-const Hamburger = () => (
-    <div
-        id="hamburger"
-        onClick={() => {
-            document
-                .getElementById('hamburger')
-                ?.classList.toggle('hamburger-toggle');
-        }}
-        className="grid place-content-center w-20 h-20 p-6 mx-auto md:hidden"
-    >
-        <div className="w-16 h-2 bg-white rounded-full transition-all duration-150 before:content-[''] before:absolute before:w-16 before:h-2 before:bg-white before:rounded-full before:-translate-y-4 before:transition-all before:duration-150 after:content-[''] after:absolute after:w-16 after:h-2 after:bg-white after:rounded-full after:translate-y-4 after:transition-all after:duration-150"></div>
-    </div>
-);
+import { useState } from 'react';
+
+const Hamburger = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div
+            onClick={() => setIsOpen((prev) => !prev)}
+            className={`${isOpen ? 'hamburger-toggle' : ''} grid place-content-center w-20 h-20 p-6 mx-auto md:hidden`}
+        >
+            <div
+                className={`
+                w-10 h-1 bg-white  rounded-full transition-all duration-150 
+                before:content-[''] before:absolute 
+                before:w-10 before:h-1 before:bg-white before:rounded-full before:-translate-y-3 before:transition-all before:duration-150 
+                after:content-[''] after:absolute 
+                after:w-10 after:h-1 after:bg-white after:rounded-full after:translate-y-3 after:transition-all after:duration-150
+                `}
+            ></div>
+        </div>
+    );
+};
 
 export { Hamburger };
