11dbe073a1702f1b02e2695eda2014a8d67b5aca
Nikita Kolchin <kolchin.nv@gmail.com>
2024-09-11 20:21:05 +0300

    2

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     experimental: {
         serverActions: {
-            allowedOrigins: ['localhost:3000'],
-            // allowedForwardedHosts: ["localhost:3000"],
+            allowedOrigins: ['127.0.0.1:3000'],
+            allowedForwardedHosts: ['127.0.0.1:3000'],
             // ^ You might have to use this property depending on your exact version.
         },
     },
