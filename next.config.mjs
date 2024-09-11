1628c04527a143b4b7e443a33d74f3f20f393cb5
Nikita Kolchin <kolchin.nv@gmail.com>
2024-09-11 20:24:24 +0300

    3

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@
 const nextConfig = {
     experimental: {
         serverActions: {
-            allowedOrigins: ['127.0.0.1:3000'],
-            allowedForwardedHosts: ['127.0.0.1:3000'],
+            allowedOrigins: ['ya-therapy.ru', '127.0.0.1:3000'],
             // ^ You might have to use this property depending on your exact version.
         },
     },
