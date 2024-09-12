fa012aeb57d149e48905a917c1fb42e168bbded8
Nikita Kolchin <kolchin.nv@gmail.com>
2024-09-12 17:32:23 +0300

    started second block

--- components/layout/format.tsx
+++ components/layout/format.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import { Section } from '@/components/shared/section';
-import { VerticalFeatureRow } from '@/components/shared/vertical-feature-row';
 import { IconList } from '../shared/icon-list';
 import { Button } from '../ui/button';
-
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
+import styles from './format.module.css';
 const Format = () => (
     <Section
         isSkewed={true}
@@ -18,36 +20,42 @@ const Format = () => (
         title="Формат работы"
         description="С помощью различных практик мы мягко и бережно погружаемся в глубины нашего сознания"
     >
-        <VerticalFeatureRow
-            title="Психологическое консультирование​"
-            description="Помощь в решении трудных жизненных ситуаций"
-            image="/assets/images/feature1.webp"
-            imageAlt="First feature alt text"
-        />
-        <VerticalFeatureRow
-            title="Профдиагностика и карьерное консультирование"
-            description="Помощь в выборе профессии и самоопределении, выявление сильных
-                        сторон и зон роста, построение карьерного трека"
-            image="/assets/images/feature2.webp"
-            imageAlt="Second feature alt text"
-            reverse
-        />
-        <VerticalFeatureRow
-            title="Тренинги и мастер-классы"
-            description="Групповая работа по различным
-                        тематикам"
-            image="/assets/images/feature3.webp"
-            imageAlt="Third feature alt text"
-        />
-        <VerticalFeatureRow
-            title="Игровые методики"
-            description="Метафорические ассоциативные карты​
-                        Игра «Лила»​ Игра «Ключ к подсознанию»​ Игра
-                        «Профориентатор» и др."
-            image="/assets/images/feature4.webp"
-            imageAlt="First feature alt text"
-            reverse
-        />
+        <Accordion allowMultiple>
+            <AccordionItem
+                className={styles.item}
+                buttonProps={{
+                    className: ({ isEnter }) =>
+                        `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}`,
+                }}
+                contentProps={{ className: styles.itemContent }}
+                panelProps={{ className: styles.itemPanel }}
+                header={
+                    <div className="text-normal text-3xl font-bold">
+                        Психологическое консультирование​
+                        <img
+                            className={styles.chevron}
+                            src={'/assets/shevron-down.svg'}
+                            alt="Chevron Down"
+                        />
+                    </div>
+                }
+            >
+                Помощь в решении трудных жизненных ситуаций
+            </AccordionItem>
+            <AccordionItem header="Профдиагностика и карьерное консультирование">
+                Помощь в выборе профессии и самоопределении, выявление сильных
+                сторон и зон роста, построение карьерного трека
+            </AccordionItem>
+
+            <AccordionItem header="Тренинги и мастер-классы">
+                Групповая работа по различным тематикам
+            </AccordionItem>
+
+            <AccordionItem header="Игровые методики">
+                Метафорические ассоциативные карты​ Игра «Лила»​ Игра «Ключ к
+                подсознанию»​ Игра «Профориентатор» и другие
+            </AccordionItem>
+        </Accordion>
     </Section>
 );
 
