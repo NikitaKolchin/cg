d4eaa34d80603e3d129222b737ad9b4c8b5d7247
Nikita Kolchin <kolchin.nv@gmail.com>
2024-09-29 16:15:28 +0300

    added nodemailer to banner

--- components/layout/banner.tsx
+++ components/layout/banner.tsx
@@ -1,20 +1,14 @@
-import Link from 'next/link';
-
 import { CTABanner } from '../shared/cta-banner';
 import { Section } from '../shared/section';
-import { Button } from '@/components/ui/button';
+import { Button } from '../ui/button';
+
 const Banner = () => (
     <Section color="bg-dark" yPadding="py-8" isSkewedPlus={true}>
-        <CTABanner
-            title="Я познакомлю вас с миром профессий и возможностей"
-            button={
-                <Link href="#">
-                    <Button size={'link'} className="w-full h-full">
-                        Записаться
-                    </Button>
-                </Link>
-            }
-        />
+        <CTABanner title="Я познакомлю вас с миром профессий и возможностей">
+            <Button size={'link'} className="w-full h-full" type="submit">
+                Записаться
+            </Button>
+        </CTABanner>
     </Section>
 );
 export { Banner };
--- components/shared/cta-banner.tsx
+++ components/shared/cta-banner.tsx
@@ -1,13 +1,38 @@
 'use client';
-import type { ReactNode } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 type CTABannerProps = {
     title: string;
-    button: ReactNode;
 };
 
-const CTABanner = (props: CTABannerProps) => {
+async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    try {
+        const response = await fetch('/api/contact', {
+            method: 'post',
+            body: formData,
+        });
+
+        if (!response.ok) {
+            console.log('falling over');
+            throw new Error(`response status: ${response.status}`);
+        }
+        const responseData = await response.json();
+        console.log(responseData['message']);
+
+        alert('Message successfully sent');
+    } catch (err) {
+        console.error(err);
+        alert('Error, please try resubmitting the form');
+    }
+}
+
+const CTABanner: FC<PropsWithChildren<CTABannerProps>> = ({
+    title,
+    children,
+}) => {
     const [inViewRef, inView] = useInView({
         threshold: 0.3,
         triggerOnce: true,
@@ -19,14 +44,16 @@ const CTABanner = (props: CTABannerProps) => {
         >
             <div className="text-xl sm:py-8 sm:w-1/2 text-center">
                 <div className="text-white sm:text-3xl max-sm:text-2xl">
-                    {props.title}
+                    {title}
                 </div>
             </div>
-            <div
+            <form
+                onSubmit={handleSubmit}
                 className={`mx-auto relative max-sm:w-full sm:w-1/2 h-12 rounded-full border border-gray-500  mt-3 sm:mt-0 ${inView ? 'max-sm:animate-bounce' : ''} transition-transform duration-500`}
             >
                 <input
                     type="email"
+                    name="email"
                     className="peer h-full w-full
                                     rounded-full 
                                     border border-blue-gray-200 
@@ -40,10 +67,9 @@ const CTABanner = (props: CTABannerProps) => {
                     required
                 />
                 <div className="absolute w-1/3 left-2/3 inset-0  rounded-full border">
-                    {' '}
-                    {props.button}
+                    {children}
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
