c97f1cbf3188bb537a415f52d3382c4e6282b897
Nikita Kolchin <kolchin.nv@gmail.com>
2024-10-09 16:46:11 +0300

    added div

--- components/shared/appointment.tsx
+++ components/shared/appointment.tsx
@@ -8,6 +8,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { FormError } from '../form-error';
 import { FormSuccess } from '../form-sucess';
+import { Button } from '../ui/button';
 
 type AppointmentProps = {
     user: {
@@ -46,48 +47,65 @@ const Appointment: FC<PropsWithChildren<AppointmentProps>> = ({ user }) => {
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label htmlFor="">Имя</label>
+                <div className="flex flex-col gap-6">
+                    <div>
+                        <div>
+                            <label htmlFor="">Имя</label>
 
-                    {errors?.name && <span>{errors.name.message}</span>}
+                            {errors?.name && <span>{errors.name.message}</span>}
+                        </div>
+                        <input
+                            {...register('name')}
+                            type="text"
+                            defaultValue={user?.name}
+                        />
+                    </div>
+                    <div>
+                        <div>
+                            <label htmlFor="">Почта</label>
+                            {errors?.email && (
+                                <span>{errors.email.message}</span>
+                            )}
+                        </div>
+                        <input
+                            {...register('email')}
+                            type="email"
+                            defaultValue={user?.email}
+                        />
+                    </div>
+                    <div>
+                        <div>
+                            <label htmlFor="">ваш запрос</label>
+                            {errors?.query && (
+                                <span>{errors.query.message}</span>
+                            )}
+                        </div>
+                        <input
+                            {...register('query')}
+                            name="query"
+                            type="text"
+                        />
+                    </div>
+
+                    <div>
+                        <div>
+                            <label htmlFor="">контактный телефон</label>
+                            {errors?.tel && <span>{errors.tel.message}</span>}
+                        </div>
+                        <input {...register('tel')} type="tel" />
+                    </div>
+                    <div>
+                        <div>
+                            <label htmlFor="">желаемая дата</label>
+                            {errors?.date && <span>{errors.date.message}</span>}
+                        </div>
+                        <input {...register('date')} type="datetime-local" />
+                    </div>
+
+                    <Button type="submit" className="button">
+                        Отправить
+                    </Button>
                 </div>
-                <input
-                    {...register('name')}
-                    type="text"
-                    defaultValue={user?.name}
-                />
-
-                <div>
-                    <label htmlFor="">Почта</label>
-                    {errors?.email && <span>{errors.email.message}</span>}
-                </div>
-                <input
-                    {...register('email')}
-                    type="email"
-                    defaultValue={user?.email}
-                />
-
-                <div>
-                    <label htmlFor="">ваш запрос</label>
-                    {errors?.query && <span>{errors.query.message}</span>}
-                </div>
-                <input {...register('query')} name="query" type="text" />
-
-                <div>
-                    <label htmlFor="">контактный телефон</label>
-                    {errors?.tel && <span>{errors.tel.message}</span>}
-                </div>
-                <input {...register('tel')} type="tel" />
-
-                <div>
-                    <label htmlFor="">желаемая дата</label>
-                    {errors?.date && <span>{errors.date.message}</span>}
-                </div>
-                <input {...register('date')} type="datetime-local" />
-
-                <button type="submit" className="button">
-                    Отправить
-                </button>
             </form>
             <FormError message={error} />
             <FormSuccess message={success} />
