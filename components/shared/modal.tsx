e0d9d1deb0921cbe47cecafd8a4f5d1b85de68b6
Nikita Kolchin <kolchin.nv@gmail.com>
2024-10-16 13:11:32 +0300

    some swiper improvement

--- components/shared/modal.tsx
+++ components/shared/modal.tsx
@@ -1,4 +1,5 @@
-import { FC, PropsWithChildren } from 'react';
+'use client';
+import { FC, PropsWithChildren, useEffect } from 'react';
 
 type Props = {
     closeWindow: () => void;
@@ -10,17 +11,24 @@ const Modal: FC<PropsWithChildren<Props>> = ({
     closeWindow,
     isOnTop = false,
 }) => {
+    useEffect(() => {
+        document.documentElement.style.overflow = 'hidden';
+
+        return () => {
+            document.documentElement.style.overflow = 'auto';
+        };
+    }, []);
     return (
         <>
             <div
                 onClick={closeWindow}
-                className={`${isOnTop ? '' : 'justify-center items-center'} flex overflow-x-hidden overflow-y-auto fixed inset-0 z-30 outline-none focus:outline-none`}
+                className={`${isOnTop ? '' : 'justify-center items-center'} flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none`}
             >
                 <div className="relative w-auto my-6 mx-auto max-w-3xl animate-fadeIn">
                     {children}
                 </div>
             </div>
-            <div className="opacity-85 fixed inset-0 z-20 bg-gray-800"></div>
+            <div className="opacity-85 fixed inset-0 z-30 bg-gray-800"></div>
         </>
     );
 };
--- components/shared/slider-images.tsx
+++ components/shared/slider-images.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -7,6 +7,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import Image from 'next/image';
 import { useBetterMediaQuery } from '@/hooks/use-better-media-query';
+import { Modal } from './modal';
 
 type Props = {
     data: Array<{
@@ -17,37 +18,66 @@ type Props = {
 
 const SliderImages: FC<Props> = ({ data }) => {
     const isMobile = useBetterMediaQuery('(max-width: 480px)');
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [fileName, setFileName] = useState('');
     return (
-        <Swiper
-            grabCursor={true}
-            centeredSlides={true}
-            spaceBetween={10}
-            slidesPerView={isMobile ? 1.15 : 1}
-            pagination={!isMobile}
-            navigation={!isMobile}
-            modules={[Pagination, Navigation]}
-        >
-            {data.map((item) => (
-                <SwiperSlide key={item.fileName}>
-                    <div className={`bg-white`}>
-                        <div className="flex justify-center">
-                            <Image
-                                className="align-middle"
-                                src={`/images/${item.fileName}.webp`}
-                                alt={item.text}
-                                width={
-                                    item.fileName === 'e4' ||
-                                    item.fileName === 'e5'
-                                        ? 820
-                                        : 880
-                                }
-                                height={600}
-                            />
+        <>
+            <Swiper
+                grabCursor={true}
+                centeredSlides={true}
+                spaceBetween={10}
+                slidesPerView={isMobile ? 1.15 : 1}
+                pagination={!isMobile}
+                navigation={!isMobile}
+                modules={[Pagination, Navigation]}
+            >
+                {data.map((item) => (
+                    <SwiperSlide key={item.fileName}>
+                        <div className={`bg-white`}>
+                            <div className="flex justify-center items-center flex-col">
+                                {!isMobile && (
+                                    <p className="text-xl max-w-3xl text-gray-800 text-center">
+                                        {item.text}
+                                    </p>
+                                )}
+                                <Image
+                                    className="align-middle"
+                                    src={`/assets/images/${item.fileName}.webp`}
+                                    alt={item.text}
+                                    width={
+                                        item.fileName === 'e4' ||
+                                        item.fileName === 'e5'
+                                            ? 820
+                                            : 880
+                                    }
+                                    height={600}
+                                    onClick={() => {
+                                        setFileName(item.fileName);
+                                        setIsOpenModal(true);
+                                    }}
+                                />
+                            </div>
                         </div>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            {isOpenModal && (
+                <Modal
+                    isOnTop={false}
+                    closeWindow={() => {
+                        setIsOpenModal(false);
+                    }}
+                >
+                    <Image
+                        className="align-middle"
+                        src={`/images/${fileName}.png`}
+                        alt={'image'}
+                        width={880}
+                        height={600}
+                    />
+                </Modal>
+            )}
+        </>
     );
 };
 
