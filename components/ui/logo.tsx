298698a7b75a673ab35f1cf13438a9874186944a
Nikita Kolchin <kolchin.nv@gmail.com>
2024-10-26 18:41:17 +0300

    improved margins

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -7,14 +7,14 @@ type LogoProps = {
 };
 
 export const Logo: FC<LogoProps> = ({ isTop }) => {
-    const size = isTop ? '64' : '24';
+    const size = isTop ? '32' : '24';
     const style = isTop
         ? 'font-regular uppercase sm:text-3xl max-sm:text-xl inline-flex items-center'
         : 'font-regular uppercase sm:text-xl max-sm:text-sm sm:flex sm:flex-col max-sm:inline-flex max-sm:items-center';
-    const imagePadding = isTop ? 'p-4' : 'py-4';
+    const imagePadding = isTop ? 'mr-4' : 'py-4';
     const textPadding = isTop ? '' : 'max-sm:pl-2';
     return (
-        <span className={` text-white ${style}`}>
+        <div className={` text-white ${style}`}>
             <Image
                 src="/images/icon.svg"
                 width={size}
@@ -23,6 +23,6 @@ export const Logo: FC<LogoProps> = ({ isTop }) => {
                 className={imagePadding}
             />
             <span className={textPadding}>{AppConfig.site_name}</span>
-        </span>
+        </div>
     );
 };
